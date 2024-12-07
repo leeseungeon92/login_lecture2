e5b35bc2304ab1b34d30e62a3b41218f491fbc0b
leeseungeon92 <oksktmddjs@gmail.com>
2024-12-07 16:28:17 +0900

    프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -10,6 +10,7 @@ const home = require("./src/routes");
 //앱 세팅
 app.set("views", "./src/views"); //1pm = view 설정, 2pm = view 경로
 app.set("view engine", "ejs") //1pm = view engine 세팅, 2pm = "ejs"파일로 설정
+app.use(express.static(`${__dirname}/src/public`)) //미들웨어 , dirname = app.js 위치를 가리킴
 
 app.use("/", home);
 
