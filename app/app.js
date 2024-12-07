ad881a26f30d94488eaad7a4007bacd1b7e73e46
leeseungeon92 <oksktmddjs@gmail.com>
2024-12-07 14:42:53 +0900

    폴더구조 최적화

--- app/app.js
+++ app/app.js
@@ -5,10 +5,10 @@ const express = require("express");
 const app = express();
 
 //라우팅
-const home = require("./routes");
+const home = require("./src/routes");
 
 //앱 세팅
-app.set("views", "./views"); //1pm = view 설정, 2pm = view 경로
+app.set("views", "./src/views"); //1pm = view 설정, 2pm = view 경로
 app.set("view engine", "ejs") //1pm = view engine 세팅, 2pm = "ejs"파일로 설정
 
 app.use("/", home);
