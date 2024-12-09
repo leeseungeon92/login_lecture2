399dff5f3315558e87a21da9cd1c853687a47553
leeseungeon92 <oksktmddjs@gmail.com>
2024-12-09 09:10:23 +0900

    회원가입 라우팅 구현

--- app/src/public/js/register.js
+++ app/src/public/js/register.js
@@ -11,11 +11,15 @@ const registerBtn = document.querySelector("#button");
 registerBtn.addEventListener("click", register);
 
 function register() {
+    if(!id.value) return alert("아이디를 입력해주세요.");
+    if(password.value !== confirmPassword.value){
+        return alert("비밀번호가 일치하지 않습니다.");
+    }
+
     const req = {
         id : id.value,
         name : name.value,
         password : password.value,
-        confirmPassword : confirmPassword.value
     };
 
     console.log(req);
--- app/src/routes/controller.js
+++ app/src/routes/controller.js
@@ -21,26 +21,12 @@ const process = {
     login : (req, res)=> {
         const user = new User(req.body);
         const response = user.login();
-        console.log(response);
         return res.json(response);
     },
 
     register : (req, res)=>{
-        const id = req.body.id,
-        password = req.body.password;
-
-        const users = UserStorage.getUsers("id","password");
-
-        const response = {};
-        if(users.id.includes(id)){
-            const idx = users.id.indexOf(id);
-            if(users.password[idx] === password){
-                response.success = true;
-                return res.json(response);
-            }
-        }
-        response.success = false;
-        response.msg = "로그인 실패";
+        const user = new User(req.body);
+        const response = user.register();
         return res.json(response);
     }
 }
